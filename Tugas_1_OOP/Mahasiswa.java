9723e680571ab4ae82709ecce5b2850912b1f459
hadi-cyber <61815942+hadi-cyber@users.noreply.github.com>
2020-03-15 07:39:37 +0700

    Add files via upload

---
+++ Tugas_1_OOP/Mahasiswa.java
@@ -0,0 +1,29 @@
+public class Mahasiswa {
+private String nim;
+private String nama;
+private Integer nilai;
+
+public Mahasiswa () {
+}
+public String getNama () {
+return nama;
+}
+public void setNama (String newNama) {
+this.nama = newNama;
+}
+public Integer getNilai () {
+return nilai;
+}
+public void setNilai (String newNilai) {
+this.nilai = newNilai;
+}
+public String getNim () {
+return nim;
+}
+public void setNim (String newNim) {
+this.nim = newNim;
+}
+public String toString () {
+return null;
+}
+}
