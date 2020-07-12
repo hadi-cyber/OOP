28bd132d47ff13df872596618eb9aa315d94b542
hadi-cyber <hadikusumanto2000@gmail.com>
2020-07-12 15:05:49 +0700

    UAS

---
+++ data-mahasiswa/src/main/java/lab/phb/datamahasiswa/model/DataMahasiswaModel.java
@@ -0,0 +1,11 @@
+package lab.phb.datamahasiswa.model;
+
+import lombok.Data;
+
+@Data
+public class DataMahasiswaModel {
+    private String nim;
+    private String nama;
+    private String jurusan;
+    private String alamat;
+}
